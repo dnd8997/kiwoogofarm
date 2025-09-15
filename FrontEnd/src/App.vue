f867139e6226cb0328a0a9724f99b2d9e460bb2f
dnd8997 <76987930+dnd8997@users.noreply.github.com>
2025-09-15 20:08:42 +0900

    Update App.vue

--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HeaderView from '@/views/HeaderView.vue'
-
+import { RouterLink, RouterView } from "vue-router";
+import HeaderView from "@/views/HeaderView.vue";
 </script>
 
 <template>
@@ -9,10 +8,21 @@ import HeaderView from '@/views/HeaderView.vue'
     <HeaderView />
     <RouterView />
   </v-app>
-  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
-  <link href="https://cdn.jsdelivr.net/npm/@mdi/font@7.x/css/materialdesignicons.min.css" rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
+    rel="stylesheet"
+  />
+  <link
+    href="https://cdn.jsdelivr.net/npm/@mdi/font@7.x/css/materialdesignicons.min.css"
+    rel="stylesheet"
+  />
 </template>
 
 <style scoped>
+@font-face {
+  font-family: "Noto Sans KR";
+  src: url("/path/to/noto-sans-kr-regular.woff2") format("woff2");
 
+  font-display: swap;
+}
 </style>
